f2a7533ce8887da9488b59c1f032b77c8a15bb2d
Dido2006 <mbigdeli442@.com>
2023-12-17 19:13:08 +0330

    build Main stracture

---
+++ rahkar-bootcamp/Dice/src/app/PlayerModel.ts
@@ -0,0 +1,22 @@
+export class Player{
+
+         userId!:number;
+         userName!:string
+         password!:string
+         score:number=0
+
+}
+
+// export class Player{
+
+//     public userId!:number;
+//     public userName!:string
+//     public password!:string
+//     public score:number=0
+//     constructor(id:number,name:string,pass:string,score:number){
+//         this.userId=id;
+//         this.userName=name;
+//         this.password=pass;
+//         this.score=score;
+//     }
+// }
