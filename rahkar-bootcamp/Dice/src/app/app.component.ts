5a0e366e0c93ac45cf412bbb366fbb46c3d23058
Dido2006 <mbigdeli442@.com>
2023-12-17 19:13:36 +0330

    build main stracture

--- rahkar-bootcamp/Dice/src/app/app.component.ts
+++ rahkar-bootcamp/Dice/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PlayGroundComponent } from './play-ground/play-ground.component';
+import { LoginComponent } from './login/login.component';
+import { GameOverComponent } from './game-over/game-over.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,PlayGroundComponent,LoginComponent,GameOverComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+StatusCode=0;
   title = 'Dice';
 }
