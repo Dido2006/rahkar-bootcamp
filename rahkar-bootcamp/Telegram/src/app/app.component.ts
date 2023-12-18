a96244f0d18e454e89743a38154eb1bc7c392b00
Dido2006 <mbigdeli442@.com>
2023-12-19 01:26:50 +0330

    removed Dice Component

--- rahkar-bootcamp/Telegram/src/app/app.component.ts
+++ rahkar-bootcamp/Telegram/src/app/app.component.ts
@@ -4,13 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { telegramService } from './srv/telegram.service';
 import { HttpClientModule } from '@angular/common/http'
 import { TelegramComponent } from "./telegram/telegram.component";
-import { DiceComponent } from "./dice/dice.component";
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, TelegramComponent, DiceComponent]
+    imports: [CommonModule, RouterOutlet, TelegramComponent]
 })
 export class AppComponent {
   title = 'Telegram';
